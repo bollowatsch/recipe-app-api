6282d72d0ca8051955a26741511ae7029027c1fc
Stephan Bollauf <stephan.bollauf@stud.fh-campuswien.ac.at>
2024-08-06 09:38:21 +0200

    change user list

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,7 +24,6 @@ class AdminSiteTests(TestCase):
             name='Test User'
         )
 
-
     def test_userts_list(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
@@ -32,3 +31,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_Edit_user_page(self):
+        """Test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
