e92f063d60807f29d2125ba370b8fd34870fafdf
Stephan Bollauf <stephan.bollauf@stud.fh-campuswien.ac.at>
2024-08-11 16:10:31 +0200

    fix typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -14,7 +14,7 @@ CREATE_USER_URL = reverse("user:create")
 
 def create_user(**params):
     """Create and return a new user."""
-    return get_user_model().object.create_user(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserApiTests(TestCase):
@@ -58,7 +58,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, res.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload["email"]
         ).exists()
