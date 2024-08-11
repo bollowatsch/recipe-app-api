a40007e4c2ef050d5f318b3b0cb259834ad3defa
Stephan Bollauf <stephan.bollauf@stud.fh-campuswien.ac.at>
2024-08-11 16:24:13 +0200

    fix typos

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,8 +33,8 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(email=payload["email"])
-        self.assertTrue(user.check_password(payload["password"]))
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn("password", res.data)
 
     def test_create_user_with_email_exists_error(self):
